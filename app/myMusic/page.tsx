cbe1026f046fae9842baf2c421e4987930fe5f49
Ziyu Chen <ch.ziyu@outlook.com>
2024-02-27 21:14:31 +0800

    update

--- app/myMusic/page.tsx
+++ app/myMusic/page.tsx
@@ -1,5 +1,58 @@
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+
+  const likedSong=[{name:"爱是昂贵的",singer:"声音玩具",album:"劳动之余",time:'08:06'},{name:"秋分",singer:"蛙池",album:"郊游",time:'04:05'},{name:"爱是昂贵的",singer:"声音玩具",album:"劳动之余",time:'3:05'}]
+
 const Mymusic=()=> {
-    return<a>我的音乐</a>
+    return<div>
+        <div >
+            喜欢的音乐
+        </div>
+        <div><img src='/images/oheart.avif' className='w-[200px]'/></div>
+        <div className="bg-gradient-to-r from-cyan-500 to-blue-500">
+             <Avatar className=''>
+              <AvatarImage src="/images/头像.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            </div>
+            <div>用户名</div>
+        <div>
+<Table >
+  <TableCaption>你喜欢的音乐</TableCaption>
+  <TableHeader>
+    <TableRow>
+    <TableHead className="w-[100px]"></TableHead>
+      <TableHead>歌曲</TableHead>
+      <TableHead>歌手</TableHead>
+      <TableHead>专辑</TableHead>
+      <TableHead className="text-right">时长</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+  {likedSong.map((item,index) => (
+
+          <TableRow key={index}>
+            <TableCell>{index+1}</TableCell>
+            <TableCell className="font-medium">{item.name}</TableCell>
+            <TableCell>{item.singer}</TableCell>
+            <TableCell>{item.album}</TableCell>
+            <TableCell className="text-right">{item.time}</TableCell>
+          </TableRow>
+        ))}
+  </TableBody>
+</Table>
+        </div>
+
+    </div>
 }
 
 export default Mymusic
--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,209 @@
 // import '@/app/globals.css'
-import styles from '@/app/ui/home.module.css';
-import { Toggle } from "@/components/ui/toggle"
+"use client"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import * as React from "react"
+import Autoplay from "embla-carousel-autoplay"
 
-const titles = [{name:'发现音乐',link:'/discover'},{name:'我的音乐',link:'/myMusic'},{name:'歌手',link:'/singerFilter'},{name:'成为音乐人',link:'/becomeSinger'}]
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command"
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Label } from "@/components/ui/label"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+const titles = [{ name: '发现音乐', link: '/discover' }, { name: '我的音乐', link: '/myMusic' }, { name: '歌手', link: '/singerFilter' }, { name: '成为音乐人', link: '/becomeSinger' }]
+const pictures = ["https://plus.unsplash.com/premium_photo-1664302427357-40eb7c8fd3c0?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bXVzaWN8ZW58MHx8MHx8fDA%3D", "https://images.unsplash.com/photo-1599467556385-48b57868f038?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDB8fHNpbmdlcnxlbnwwfHwwfHx8MA%3D%3D", "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bXVzaWN8ZW58MHx8MHx8fDA%3D", "https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fG11c2ljfGVufDB8fDB8fHww"]
+const musicRec = [{ name: '鲜花', pic: '/images/鲜花.jpeg' },
+{ name: '鲜花', pic: '/images/鲜花.jpeg' },
+{ name: '鲜花', pic: '/images/鲜花.jpeg' },
+{ name: '鲜花', pic: '/images/鲜花.jpeg' }, { name: '鲜花', pic: '/images/鲜花.jpeg' }, { name: '鲜花', pic: '/images/鲜花.jpeg' }, { name: '鲜花', pic: '/images/鲜花.jpeg' }, { name: '鲜花', pic: '/images/鲜花.jpeg' },]
+const newRec = [{ name: '鲜花', pic: '/images/鲜花.jpeg' },
+{ name: '鲜花', pic: '/images/鲜花.jpeg' },
+{ name: '鲜花', pic: '/images/鲜花.jpeg' },
+{ name: '鲜花', pic: '/images/鲜花.jpeg' }, { name: '鲜花', pic: '/images/鲜花.jpeg' },
+{ name: '鲜花', pic: '/images/鲜花.jpeg' }, { name: '鲜花', pic: '/images/鲜花.jpeg' }]
 
 export default function Home() {
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  )
   return (
     <div>
-      {/* <p className="text-blue-500">Home</p> */}
-      {/* <div
-        className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
-      /> */}
       <div className="flex flex-row mt-5 justify-between items-center">
-        <div className="flex flex-row w-90 leading-[40px] md:font-bold ml-[60px] text-lg "><img src='/images/orange.png' className='h-10 mr-[10px]'/>橘子音乐</div>
-        {/* <p className="hover:bg-sky-700 p-3">发现音乐</p> */}
+        <div className="flex flex-row w-90 leading-[40px] md:font-bold ml-[60px] text-lg "><img src='/images/orange.png' className='h-10 mr-[10px]' />橘子音乐</div>
         <div className="w-200 flex flex-row justify-between gap-x-3 items-center ">
-        {
-          titles.map(item=>{
-            return(
-              <a className="hover:bg-orange-200 px-6 py-3" href={item.link} >{item.name}</a>
-            )
-          })
-        }
-</div>
+          {
+            titles.map(item => {
+              return (
+                <a className="hover:bg-orange-200 px-6 py-3" href={item.link} >{item.name}</a>
+              )
+            })
+          }
+        </div>
         <div className="flex w-full max-w-sm items-center space-x-2 ">
-      <Input type="email" placeholder="周杰伦" />
-      <Button type="submit">搜索</Button>
-    </div>
-    <div>
-    <Avatar className='mr-[70px]'>
-  <AvatarImage src="/images/头像.png" />
-  <AvatarFallback>CN</AvatarFallback>
-</Avatar>
-</div>
-
+          <Input type="email" placeholder="周杰伦" />
+          <Button type="submit">搜索</Button>
+        </div>
+        <div className="flex flex-row gap-x-2">
+          {/* 条件判断是否有头像 */}
+         <Avatar className=''>
+              <AvatarImage src="/images/头像.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <Dialog>
+      <DialogTrigger asChild className="mr-11">
+        <Button variant="outline">登录</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>登录</DialogTitle>
+          <DialogDescription>
+            请输入账号和密码登录
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-3 py-4">
+          <div className="flex flex-row items-center gap-3">
+            <Input
+              id="name"
+              defaultValue="支持qq/邮箱/手机号码登录"
+              className="col-span-3"
+            />
+          </div>
+          <div className=" items-center gap-3">
+            <Input
+              id="username"
+              defaultValue="请输入密码"
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">登录</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+          {/* <HoverCard>
+            <HoverCardTrigger>
+              <Avatar className='mr-[70px]'>
+                <AvatarImage src="/images/头像.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </HoverCardTrigger>
+            <HoverCardContent className="w-55">
+              <Command >
+                <CommandList >
+                  <CommandGroup heading="用户名">
+                    <CommandItem ><a href='/myMusic'>我的主页</a></CommandItem>
+                    <CommandItem>设置</CommandItem>
+                    <CommandItem>退出登录</CommandItem>
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </HoverCardContent>
+          </HoverCard> */}
+        </div>
       </div>
-      
-      {/* <div className={styles.shape}>233</div> */}
+      {/* 轮播图 */}
+
+      <div className=' w-full mt-[8px]'>
+        <Carousel
+          plugins={[plugin.current]}
+          className=""
+          onMouseEnter={plugin.current.stop}
+          onMouseLeave={plugin.current.reset}
+        >
+          <CarouselContent>
+            {pictures.map((item, index) => (
+              <CarouselItem >
+                <div className="">
+                  <Card className='border-white'>
+                    <CardContent className="p-0">
+                      <img src={item} className='w-full h-80 object-cover' />
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          {/* <CarouselPrevious />
+          <CarouselNext /> */}
+        </Carousel>
+      </div>
+      {/* 歌曲推荐 */}
+      <div className='flex items-center flex-col ml-60'>
+        <div>
+          <div className='py-3 px-1 text-lg flex flex-row w-full'> <img src='/images/music.png' className='w-6 h-6 mr-[7px]' />歌曲推荐</div>
+          <div className='flex flex-row flex-wrap gap-x-6 w-9/12 ml-1/12'>
+            {
+              musicRec.map(item => {
+                return (
+                  <div className="h-50 mb-2 ">
+                    <img src={item.pic} className='rounded h-48' />
+                    <a className='ml-[3px]'>{item.name}</a>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
+      {/* 新歌上架 */}
+      <div className=''>
+        <div className='py-3 px-1 text-lg flex flex-row ml-60'> <img src='/images/music.png' className='w-6 h-6 mr-[7px]' /> 新歌上架</div>
+        <div className='w-[770px] border-gray-100 border-[1px] p-2 rounded ml-[280px]'>
+          <Carousel
+            opts={{
+              align: "start",
+            }}
+            className=""
+          >
+            <CarouselContent>
+              {newRec.map((item, index) => (
+                <CarouselItem key={index} className="lg:basis-1/4">
+                  <div className="p-1">
+                    <Card>
+                      <img src={item.pic} className='' />
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+      </div>
     </div>
+
   );
 }
